8e9a8579dd59d5344471944fc0a3be9a5e23cd6a
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-10-13 16:28:01 -0700

    latest update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
-#!/bin/bash
+# !/ bin / bash
 #include <stdio.h>
-
-
-
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+int main(void)
+{
+puts("Programming is like
+building a multilingual puzzle\n");
+return (0);
+}

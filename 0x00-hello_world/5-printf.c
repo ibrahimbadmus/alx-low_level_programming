4ec197730f90426291ace448cea066c7db874ec7
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-10-13 17:00:24 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
-#!/bin/bash
- #include <stdio.h>
-int main(void) 
-{ printf("with proper grammar, but the outcome is a piece of art,\n"); 
-	return (0); }
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome
+is a piece of art,\n");
+	return (0);
+}

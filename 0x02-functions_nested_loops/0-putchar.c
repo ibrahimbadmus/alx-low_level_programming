c079d425bd8255756c10c7c9c330ee06565aa205
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-10-18 19:20:52 +0100

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "highbee.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "highbee";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}

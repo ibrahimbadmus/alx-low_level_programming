2c4112690b0e4428d0aab63ae42bdbb41359f3a5
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-10-24 23:46:34 +0100

    task upgrade

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,29 @@
 #include "main.h"
-
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * _atoi - this function converts a string to integer
+ * @s: the string for convert to integer
+ * Return: this funtion returns the result
  */
-int _atoi(char*s)
+int _atoi(char *s)
 {
+	int i, sign;
+	unsigned int number;
 
-	return (0);
+	sign = 1;
+	i = 0;
+	number = 0;
+
+
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while ((s[i] >= '0') && (s[i] <= '9'))
+	{
+		number = number * 10 + (s[i] - '0');
+		i++;
+	}
+	return (number * sign);
 }

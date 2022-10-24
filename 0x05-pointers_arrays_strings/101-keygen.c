e52a3f4c057abed324605d6b0229bb6b7d3dd8c8
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-10-24 23:48:18 +0100

    addtional task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,13 +1,33 @@
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+  * main - entry point
+  * Description: creates a random key for program 101_crackme
+  * Return: always 0
+  */
 int main(void)
 {
+	    int sum, i, r;
 
-	return (0);
+		char decode[27] = "abcdefghijklmnopqrstuvwxyz";
+		char key[30];
+
+		sum = 0;
+		i = 0;
+
+		srand(time(NULL));
+
+		while (sum < 2772)
+		{
+			r = rand() % 10;
+			key[i] = decode[r];
+			sum += key[i];
+			i++;
+		}
+		r = 2772 - sum;
+		key[i] = r;
+		printf("%s\n",  key);
+		return (0);
 }

b6c3c3d0f1ba1d6453b1e3063cee743aefc192f2
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-11-08 15:07:30 +0100

    correction on task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,26 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
 /**
- * main - prints a number, followed by a new line
- * @argv: array of pointers to strings
- * @argc: argument count
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
  * Return: 0
  */
-int main(int argc, __attribute__ ((unused)) char *argv[])
+
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+
 	return (0);
 }

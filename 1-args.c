fde58f4abfb5ff32a004bbe5d7de103215a3bf91
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-11-08 15:00:07 +0100

    task 01 correction

--- 1-args.c
+++ 1-args.c
@@ -1,26 +1,13 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
- * main - Prints the number of arguments passed into main
- * @argc: Number of command line arguments
- * @argv: Array name
+ * main - prints a number, followed by a new line
+ * @argv: array of pointers to strings
+ * @argc: argument count
  * Return: 0
  */
-
-int main(int argc, char *argv[])
+int main(int argc, __attribute__ ((unused)) char *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
-
+	printf("%d\n", argc - 1);
 	return (0);
 }

3cfb3f25fda006f42815d3e197154db4f0f53188
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-11-16 16:18:21 +0100

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,37 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * main - Print the opcodes of the function
- * @argc: Number of args given
- * @argv: Argument of number of bytes to print
- *
- * Return: 0 on success, Print Error for errors, exit value 1 if
- * incorrect # of args, exit value 2 if number of bytes is negative
- */
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
 int main(int argc, char *argv[])
 {
-	char *add;
-	int i, bytes;
+	int i, n;
+
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
-	if (atoi(argv[1]) < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	add = (char *)&main;
-	bytes = atoi(argv[1]);
-	i = 0;
-	while (i < bytes - 1)
+
+	for (i = 0; i < n; i++)
 	{
-		printf("%02hhx ", add[i]);
-		i++;
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
 	}
-	printf("%02hhx\n", add[i]);
 	return (0);
 }

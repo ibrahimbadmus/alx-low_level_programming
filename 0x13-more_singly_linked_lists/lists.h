4e099dcb935e06bad16292fbd6821c5a800b28c2
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-11-28 22:14:12 +0100

    correction on lists h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,43 +1,22 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-/*
- * File: lists.h
- * Auth: Badmus Ibrahim Olaiya
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
- */
-
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: pointer to the first element of the list.
+ * @n: int to set in the new node.
+ * Return: address of the new element, or NULL if it failed
+ **/
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	listint_t *new;
 
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-void free_listint2(listint_t **head);
-int pop_listint(listint_t **head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-#endif /* LISTS_H */
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}

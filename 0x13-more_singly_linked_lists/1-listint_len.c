9152697381a8418f5f78c83a328f08765dd831b3
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-11-28 22:16:02 +0100

    correction of list int len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,26 +1,20 @@
-/*
- * File: 1-listint_len.c
- * Auth: Badmus Ibrahim
- */
-
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * listint_len - Returns the number of elements
- *               in a linked listint_t list.
- * @h: A pointer to the head of the listint_t list.
- *
- * Return: The number of elements in the listint_t list.
- */
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
+ **/
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes = 0;
+	const listint_t *node = h;
+	size_t cont = 0;
 
-	while (h)
+	while (node)
 	{
-		nodes++;
-		h = h->next;
+		cont++;
+		node = node->next;
 	}
 
-	return
+	return (cont);
+}

5692b6e24ca833f34ce327944a14797fd14027cf
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-11-28 22:19:27 +0100

    correction on list int

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,27 @@
+/*
+ * File: 0-print_listint.c
+ * Auth: Badmus Ibrahim
+ */
+
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * listint_len - returns the number of elements in a linked listint_t list.
- * @h: pointer to the list.
- * Return: number of nodes.
- **/
-size_t listint_len(const listint_t *h)
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the list_t list.
+ *
+ * Return: The number of nodes in the list_t list.
+ */
+size_t print_listint(const listint_t *h)
 {
-	const listint_t *node = h;
-	size_t cont = 0;
+	size_t nodes = 0;
 
-	while (node)
+	while (h)
 	{
-		cont++;
-		node = node->next;
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 
-	return (cont);
+	return (nodes);
 }
